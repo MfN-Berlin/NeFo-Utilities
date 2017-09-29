86b2cb9f3638a7a18737d3bc588f1f725e3626d4
AntonioPonte <Antonio.Ponte@mfn-berlin.de>
2017-09-29 11:38:56 +0200

    #399: NeFo-Produkte Bericht, Faktenblatt, Stellungnahme und Studie in NeFo-Dokument umwandeln

---
+++ nefo_cmd_tools/nefo_convert_factsheet_document.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * NeFo-Produkt Faktenblatt
+ */
+
+$source_node_type = 'nefo_product_factsheet';
+$category_tid = 77;
+$backup_file = '/local/apache/nefo/html/sites/all/modules/custom/nefo_utilities/nefo_cmd_tools/'. $source_node_type .'.json';
+
+$dest_node_type = 'nefo_product_document';
+$no_fields_flag = FALSE;
+$hook_options = NULL;
+$dest_fields = array();
+$dest_fields[0] = 'field_nefo_product_field_type';
+$dest_fields[1] = 'field_nefo_product_document';
+$dest_fields[2] = 'field_nefo_product_link';
+$dest_fields[3] = 'field_nefo_description';
+$dest_fields[4] = 'field_nefo_product_icon';
+$dest_fields[5] = 'field_nefo_taxonomy_tags';
+
+// Create backup
+$backup_nodes = array();
+$nodes = node_load_multiple(NULL, array('type' => $source_node_type));
+foreach ($nodes as $nid => $node) {
+  $backup_nodes[$nid] = array(
+      'status' => $node->status,
+      'uid' => $node->uid,
+      'created' => $node->created,
+      'alias' => drupal_get_path_alias('node/'. $nid),
+  );
+}
+$content = json_encode($backup_nodes);
+file_put_contents($backup_file, $content);
+
+// Convert nodes
+$source_fields= $dest_fields;
+$source_fields[1] = 'field_nefo_factsheet';
+$source_fields[2] = 'field_nefo_factsheet_link';
+$nodes = node_load_multiple(NULL, array('type' => $source_node_type));
+foreach ($nodes as $nid => $node) {
+  $result = node_convert_node_convert($nid, $dest_node_type, $source_fields, $dest_fields, $no_fields_flag, $hook_options);
+  node_convert_messages($result, array('nid' => $nid));
+  $node_wrapper = entity_metadata_wrapper('node', $nid);
+  $node_wrapper->field_nefo_taxonomy_document = $category_tid;
+  $node_wrapper->save();
+}
+
+// Path alias
+$backup_nodes = json_decode(file_get_contents($backup_file), TRUE);
+foreach ($backup_nodes as $nid => $backup_node) {
+  $current_alias =  drupal_get_path_alias('node/'. $nid);
+  if ($current_alias !== $backup_node['alias']) {
+    $path = array('source' => "node/$nid", 'alias' => $backup_node['alias']);
+    path_save($path);
+  }
+}
+
+?>
